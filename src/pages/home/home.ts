2bd4bff92ce3d09cc5867786d0a383e32edf34bd
Ryan LaClair <ryanlaclair@gmail.com>
2018-10-13 16:53:12 -0400

    tied brewery cards to map markers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Slides } from 'ionic-angular';
 
 import { GeocodingProvider } from '../../providers/geocoding/geocoding';
 import { BreweryMappingProvider, BeerSpot } from '../../providers/brewery-mapping/brewery-mapping';
@@ -12,6 +12,8 @@ import { GoogleMapsProvider } from '../../providers/google-maps/google-maps';
 
 export class HomePage {
 
+  @ViewChild(Slides) slides: Slides;
+
   breweries: Array<BeerSpot> = new Array();
 
   constructor(public navCtrl: NavController, private geocodingProvider: GeocodingProvider, private breweryProvider: BreweryMappingProvider, private mapsProvider: GoogleMapsProvider ) { }
@@ -20,18 +22,32 @@ export class HomePage {
     this.getBreweries();
   }
 
+  handleMarkerClick(id: number) {
+    let index = this.breweries.indexOf(this.breweries.find((b) => {
+      return b.id == id;
+    }));
+
+    this.slides.slideTo(index);
+  }
+
+  handleSlideChange() {
+    let brewery = this.breweries[this.slides.getActiveIndex()];
+    
+    this.mapsProvider.showMarker(brewery.id);
+  }
+
   private getBreweries() {
     this.geocodingProvider.getCityState().then((cityState: any) => {
       this.breweryProvider.getBreweriesInCity(cityState.city).subscribe((cityBrewery: BeerSpot) => {
-        this.addBrewery(cityBrewery);
-
-        this.mapsProvider.addMarker(cityBrewery.location.latitude, cityBrewery.location.longitude);
+        if (this.addBrewery(cityBrewery)) {
+          this.mapsProvider.addMarker(cityBrewery.location.latitude, cityBrewery.location.longitude, cityBrewery.id, this.handleMarkerClick.bind(this));
+        }
       });
 
       this.breweryProvider.getBreweriesInState(cityState.state).subscribe((stateBrewery: BeerSpot) => {
-        this.addBrewery(stateBrewery);
-
-        this.mapsProvider.addMarker(stateBrewery.location.latitude, stateBrewery.location.longitude);
+        if (this.addBrewery(stateBrewery)) {
+          this.mapsProvider.addMarker(stateBrewery.location.latitude, stateBrewery.location.longitude, stateBrewery.id, this.handleMarkerClick.bind(this));
+        }
       });
     });
   }
@@ -47,6 +63,11 @@ export class HomePage {
       this.breweries.sort((a, b) => {
         return a.distance - b.distance;
       });
+
+      return true;
+    }
+    else {
+      return false;
     }
   }
 
--- src/providers/google-maps/google-maps.ts
+++ src/providers/google-maps/google-maps.ts
@@ -1,73 +1,91 @@
 import { Injectable } from '@angular/core';
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation';
 
 @Injectable()
 export class GoogleMapsProvider {
 
-  mapElement: any;
-  map: any;
-  userPosition: any;
-  userMarker: any;
+  map: google.maps.Map;
+  userPosition: Geoposition;
 
-  breweryMarkers: Array<any> = new Array();
+  markers: Array<google.maps.Marker> = new Array();
 
   constructor(private geolocation: Geolocation) { }
 
-  initializeMap(mapElement: any): Promise<any> {
-    this.mapElement = mapElement;
+  initializeMap(mapElement: any) {
+    this.geolocation.getCurrentPosition().then((position) => {
+      this.userPosition = position;
 
-    return new Promise((resolve) => {
-      this.geolocation.getCurrentPosition().then((position) => {
-        this.userPosition = position;
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        fullscreenControl: false,
+        streetViewControl: false
+      }
 
-        let mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          mapTypeControl: false,
-          fullscreenControl: false,
-          streetViewControl: false
-        }
-
-        this.map = new google.maps.Map(this.mapElement, mapOptions);
-
-        let markerOptions = {
-          position: latLng,
-          map: this.map,
-          clickable: false,
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 5,
-            strokeColor: 'red'
-          }
-        }
-
-        this.userMarker = new google.maps.Marker(markerOptions);
-
-        resolve(true);
-      });
-    });
-  }
-
-  addMarker(latitude: number, longitude: number): Promise<any> {
-    return new Promise((resolve) => {
-      let latLng = new google.maps.LatLng(latitude, longitude);
+      this.map = new google.maps.Map(mapElement, mapOptions);
 
       let markerOptions = {
         position: latLng,
         map: this.map,
-        clickable: false
+        clickable: false,
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 5,
+          strokeColor: 'blue'
+        }
       }
 
-      this.breweryMarkers.push(new google.maps.Marker(markerOptions));
-      resolve(true);
+      new google.maps.Marker(markerOptions);
     });
+  }
+
+  addMarker(latitude: number, longitude: number, id: number, clickHandler: Function) {
+    let latLng = new google.maps.LatLng(latitude, longitude);
+
+    let markerOptions = {
+      position: latLng,
+      map: this.map,
+      clickable: true,
+      id: id
+    }
+
+    let marker = new google.maps.Marker(markerOptions);
+    this.markers.push(marker);
+
+    google.maps.event.addListener(marker, 'click', () => {
+      clickHandler(marker.get('id'));
+      this.bounceMarker(marker);
+    });
+  }
+
+  showMarker(id: number) {
+    let marker: google.maps.Marker = this.markers.find((marker: google.maps.Marker) => {
+      return marker.get('id') == id;
+    });
+
+    let bounds = new google.maps.LatLngBounds();
+
+    bounds.extend(new google.maps.LatLng(this.userPosition.coords.latitude, this.userPosition.coords.longitude));
+    bounds.extend(marker.getPosition());
+
+    this.map.fitBounds(bounds);
+    this.bounceMarker(marker);
   }
 
   getUserPosition() {
     return this.userPosition;
+  }
+
+  private bounceMarker(marker: google.maps.Marker) {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+
+    setTimeout(() => {
+      marker.setAnimation(null);
+    }, 1400);
   }
 
 }
